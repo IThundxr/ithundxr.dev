9bd6e3eb53fa9d036639f73c36d8409f4d00bb2a
IThundxr <me@ithundxr.dev>
2025-09-16 22:22:50 -0400

    Projects page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   themeColor: "black",
   colorScheme: "dark",
-}
+};
 
 export default async function RootLayout({
   children,
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,48 +1,59 @@
 import { ArrowLeftIcon } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
-// TODO - Work on colors a bit
-// Also make a custom scrollbar for this page
 export default function Projects() {
   const projects = [
     {
       id: 1,
-      title: "Create Fabric",
+      title: "Create",
+      image:
+        "https://github.com/Creators-of-Create/Create/blob/9517566e46a22fc2833838feefd0e061c66186cb/.idea/icon.png?raw=true",
       description: "Building Tools and Aesthetic Technology",
-      link: "https://modrinth.com/mod/create-fabric",
-      tags: ["Fabric Mod"],
+      link: "https://modrinth.com/mod/create",
+      tags: ["170 Million Downloads", "NeoForge Mod", "Fabric Mod"],
     },
     {
       id: 2,
       title: "Create Steam n' Rails",
+      image:
+        "https://github.com/Layers-of-Railways/Railway/blob/19cacac4ee275903d954a2af694b28d05a851cf9/.idea/icon.png?raw=true",
       description: "Adding depth to Create's rail network & steam system",
       link: "https://modrinth.com/mod/create-steam-n-rails",
-      tags: ["Fabric/NeoForge Mod"],
+      tags: ["50 Million Downloads", "Fabric/NeoForge Mod"],
     },
     {
       id: 3,
       title: "Quark",
+      image:
+        "https://github.com/VazkiiMods/Quark/blob/b866a51964bb4074bc2d8a6e242a65414d0e7825/.idea/icon.png?raw=true",
       description:
         "A Quark is a very small thing. This mod is a collection of small things...",
       link: "https://modrinth.com/mod/quark",
-      tags: ["Forge Mod"],
+      tags: ["220 Million Downloads", "Forge Mod"],
     },
   ];
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start px-4 py-16">
+    <div className="flex min-h-screen flex-col items-center justify-start px-4 py-8">
       <div className="container max-w-5xl">
-        <h1 className="mb-2 text-4xl font-bold tracking-tight">Projects</h1>
-        <p className="mb-12 text-secondary">
-          A collection of my recent Minecraft mods and other projects
-        </p>
+        <h1 className="mb-8 text-4xl font-bold tracking-tight">Projects</h1>
 
         <div className="grid gap-6">
           {projects.map((project) => (
             <div
               key={project.id}
-              className="hover:border-elementBorderHover group relative overflow-hidden rounded-lg border border-elementBorder bg-element p-6 shadow-md transition-all duration-300 hover:-translate-y-0.5 hover:bg-elementHover hover:shadow-lg"
+              className="hover:border-elementBorderHover group relative overflow-hidden rounded-3xl border border-elementBorder bg-element p-6 shadow-md transition-all duration-300 hover:-translate-y-0.5 hover:bg-elementHover hover:shadow-lg"
             >
+              <div className="flex flex-row items-start gap-6">
+                <Image
+                  src={project.image}
+                  alt="Project Icon"
+                  width={96}
+                  height={96}
+                  className="rounded-xl"
+                ></Image>
+
               <div className="flex flex-col gap-2">
                 <h3 className="text-2xl font-semibold tracking-tight">
                   {project.title}
@@ -60,6 +71,7 @@ export default function Projects() {
                   ))}
                 </div>
               </div>
+              </div>
 
               <a
                 href={project.link}
@@ -73,7 +85,7 @@ export default function Projects() {
           ))}
         </div>
 
-        <div className="mt-16 text-center">
+        <div className="mt-8 text-center">
           <Link
             href="/"
             className="perspective-1000 preserve-3d inline-flex items-center gap-2 rounded-full bg-elementAccent px-8 py-3 text-lg font-medium text-background shadow-lg transition-all duration-150 ease-out will-change-transform hover:scale-[1.02] hover:bg-elementAccentHover hover:shadow-xl active:translate-y-0.5 active:scale-95 active:bg-elementAccentHover active:shadow-md"
